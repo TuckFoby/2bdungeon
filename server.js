b6911c37f64a3a0c8b850f14473b58b9eb0b0f87
TuckFoby <149215134+TuckFoby@users.noreply.github.com>
2025-04-04 01:27:21 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -386,67 +386,6 @@ io.on('connection', (socket) => {
     });
 });
 
-
-// io.on('connection', (socket) => {
-//     const req = socket.request;
-
-//     // session-based chat history
-//     if (!req.session.chatHistory) {
-//         req.session.chatHistory = [];
-//     }
-
-//     // send existing chat history to the client upon connection
-//     socket.emit('chat history', req.session.chatHistory);
-
-//     socket.on('chat message', ({ username, message }) => {
-//         if (message.length > MAX_MESSAGE_LENGTH || !ALLOWED_PATTERN.test(message)) {
-//             console.log('Blocked message.');
-//             return;
-//         }
-
-//         const chatEntry = { username, message, timestamp: new Date() };
-//         req.session.chatHistory.push(chatEntry);
-
-//         // save session changes
-//         req.session.save((err) => {
-//             if (err) console.error('Session save error:', err);
-//         });
-
-//         io.emit('chat message', chatEntry);
-//     });
-
-//     socket.on('roll message', ({ username, target, rolls, successes, timestamp }) => {
-//         // build the roll output string
-//         const rollsOutput = rolls.map((roll, index) => `Dice ${index + 1} = ${roll}`).join(', ');
-    
-//         // create a sanitized message string
-//         const message = `🎲 Target number is ${target}. Rolls are ${rollsOutput}. Successes are ${successes}`;
-    
-//         //check if the message is too long or has bad characters
-//         //console.log(message.length)
-//         if (message.length > MAX_MESSAGE_LENGTH || !ALLOWED_PATTERN.test(message)) {
-//             console.log('Blocked roll message.');
-//             return;
-//         }
-    
-//         // Create chat entry
-//         const chatEntry = { username, message, timestamp: new Date(timestamp) };
-    
-//         // Add to chat history
-//         req.session.chatHistory.push(chatEntry);
-    
-//         // Save session and broadcast
-//         req.session.save((err) => {
-//             if (err) console.error('Session save error:', err);
-//         });
-    
-//         io.emit('chat message', chatEntry); // use the same event to display in chat box
-//     });    
-//     socket.on('disconnect', () => {
-//         console.log('A user disconnected.');
-//     });
-// });
-
 app.use((err, req, res, next) => {
     if (err.code === 'EBADCSRFTOKEN') {
         console.warn('Invalid CSRF token');
